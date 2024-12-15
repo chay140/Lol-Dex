b5dad9b003283ebc19e8de9a934388165a02f487
chay140 <chaihyunpark140@gmail.com>
2024-12-16 01:17:55 +0900

    feat: ChampionDetail 타입 정의

--- src/types/Champion.ts
+++ src/types/Champion.ts
@@ -1,32 +1,77 @@
 interface Champion {
-  version: string;
   id: string;
   key: string;
   name: string;
   title: string;
   blurb: string;
-  info: {
-    [infoKey: string]: number;
-  };
-  image: {
-    full: string;
-    sprite: string;
-    group: string;
-    x: number;
-    y: number;
-    w: number;
-    h: number;
-  };
   tags: string[];
   partype: string;
-  stats: {
-    [statKey: string]: number;
+  info: ChampionInfo;
+  image: ChampionImage;
+  stats: ChampionStats;
+  version: string;
+}
+
+interface ChampionDetail extends Omit<Champion, "version"> {
+  skins: ChampionSkin[];
+  lore: string;
+  allytips: string[];
+  enemytips: string[];
+  spells: ChampionSpell[];
+  passive: {
+    name: string;
+    description: string;
+    image: ChampionImage;
   };
+  recommended: [];
 }
 
-interface ChampionDetail {
-
-
+interface ChampionImage {
+  full: string;
+  sprite: string;
+  group: string;
+  x: number;
+  y: number;
+  w: number;
+  h: number;
 }
 
+interface ChampionInfo {
+  [infoKey: string]: number;
+}
 
+interface ChampionStats {
+  [statKey: string]: number;
+}
+
+interface ChampionSkin {
+  id: string;
+  num: number;
+  name: string;
+  chromas: boolean;
+}
+
+interface ChampionSpell {
+  id: string;
+  name: string;
+  description: string;
+  tooltip: string;
+  leveltip: {
+    [leveltipCode: string]: string[];
+  };
+  maxrank: number;
+  cooldown: number[];
+  cooldownBurn: string;
+  cost: number[];
+  costBurn: string;
+  datavalues: {}; // 빈 객체
+  effect: (null | number[])[];
+  effectBurn: (null | string)[];
+  vars: [];
+  costType: string;
+  maxammo: string;
+  range: number[];
+  rangeBurn: string;
+  image: ChampionImage;
+  resource: string;
+}
