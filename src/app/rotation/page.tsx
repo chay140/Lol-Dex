dd459e5b61d8190ac8017e685944eb0ce760b365
chay140 <chaihyunpark140@gmail.com>
2024-12-19 11:52:41 +0900

    feat: newbie 로테이션 추가

--- src/app/rotation/page.tsx
+++ src/app/rotation/page.tsx
@@ -2,7 +2,7 @@
 
 import ChampionCard from "@/components/champions/ChampionCard";
 import GridSkeleton from "@/components/champions/ChampionGridSkeleton";
-import { getChampionRotation } from "@/utils/riotApi";
+import { getChampionRotation, getNewbieChampionRotation } from "@/utils/riotApi";
 import { delay } from "@/utils/utilConst";
 // CRS는 build && start
 // dev에서 하면 터미널에서도 콘솔이 찍힘
@@ -10,29 +10,56 @@ import { delay } from "@/utils/utilConst";
 import { useQuery } from "@tanstack/react-query";
 
 const delayedGetChampionRotation = async () => {
-  await delay(1500); // 3초 딜레이 줘보기 (로딩 보여주기 용)
+  await delay(100); // 1.5초 딜레이 줘보기 (로딩 보여주기 용)
   return getChampionRotation();
 };
+
+const delayedNewbieChampionRotation = async () => {
+  await delay(2000); // 로딩 보여주기용
+  return getNewbieChampionRotation();
+}
 
 const RotationPage = () => {
   const {
     data: rotations,
-    isPending,
-    error,
+    isPending: rotPending,
+    error: rotError,
   } = useQuery({
     queryKey: ["rotations"],
     queryFn: delayedGetChampionRotation,
   });
 
-  if (error) return <div>에러 발생 새로고침 시도</div>;
+  const {
+    data: newbieRotations,
+    isPending: newbiePending,
+    error: newbieError,
+  } = useQuery({
+    queryKey: ["newbieRotations"],
+    queryFn: delayedNewbieChampionRotation,
+  });
+
+  if (rotError || newbieError) return <div>에러 발생 새로고침 시도</div>;
+
+  const { rotationChamps: newbieRotationChamps, maxLevel } = newbieRotations || {};
 
   return (
     <div className="text-center">
       <h1 className="text-2xl font-bold mb-4">챔피언 로테이션</h1>
-      {isPending && <GridSkeleton />}
-      {!isPending && !error && (
+      {rotPending ? (
+        <GridSkeleton />
+      ) : (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 ">
           {rotations.map((champion) => (
+            <ChampionCard key={champion.id} champion={champion} />
+          ))}
+        </div>
+      )}
+      <h1 className="text-2xl font-bold my-4">뉴비 챔피언 로테이션 Max Level.{!newbiePending && maxLevel}</h1>
+      {newbiePending ? (
+        <GridSkeleton />
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 ">
+          {newbieRotationChamps?.map((champion) => (
             <ChampionCard key={champion.id} champion={champion} />
           ))}
         </div>
--- src/utils/riotApi.ts
+++ src/utils/riotApi.ts
@@ -17,3 +17,24 @@ export async function getChampionRotation(): Promise<Champion[]> {
 
   return rotationChamps;
 }
+
+export async function getNewbieChampionRotation(): Promise<{
+  rotationChamps: Champion[];
+  maxLevel: number;
+}> {
+  const champions = await fetchChampionList();
+
+  const response: Response = await fetch("/api/rotation");
+
+  if (!response.ok) {
+    const body = await response.json();
+    throw new Error(`fetchRotation error message: ${body.status.message}`);
+  }
+
+  const data: ChampionRotation = await response.json();
+  const rotationChamps: Champion[] = champions.filter((champ) =>
+    data.freeChampionIdsForNewPlayers.includes(Number(champ.key))
+  );
+
+  return { rotationChamps, maxLevel: data.maxNewPlayerLevel };
+}
